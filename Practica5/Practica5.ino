d564ba0e6b503868a24df37eb711d74f1ffd665d
Guillermo Caselin Covarrubias <guillecascov@gmail.com>
2023-05-08 18:35:57 -0600

    Create Practica5.ino

---
+++ Practica5/Practica5.ino
@@ -0,0 +1,89 @@
+int led1 = 2;
+int led2 = 3;
+int led3 = 4;
+int led4 = 5;
+int led5 = 6;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(led4,OUTPUT);
+  pinMode(led5,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite (led1,LOW);
+  digitalWrite (led2,LOW);
+  digitalWrite (led3,LOW);
+  digitalWrite (led4,LOW);
+  digitalWrite (led5,HIGH);
+  
+  delay(200);
+
+  digitalWrite (led1,LOW);
+  digitalWrite (led2,LOW);
+  digitalWrite (led3,LOW);
+  digitalWrite (led4,HIGH);
+  digitalWrite (led5,LOW);
+  
+  delay(200);
+
+  digitalWrite (led1,LOW);
+  digitalWrite (led2,LOW);
+  digitalWrite (led3,HIGH);
+  digitalWrite (led4,LOW);
+  digitalWrite (led5,LOW);
+  
+  delay(200);
+
+  digitalWrite (led1,LOW);
+  digitalWrite (led2,HIGH);
+  digitalWrite (led3,LOW);
+  digitalWrite (led4,LOW);
+  digitalWrite (led5,LOW);
+  
+  delay(200);
+
+  digitalWrite (led1,HIGH);
+  digitalWrite (led2,LOW);
+  digitalWrite (led3,LOW);
+  digitalWrite (led4,LOW);
+  digitalWrite (led5,LOW);
+  
+  delay(200);
+
+  digitalWrite (led1,LOW);
+  digitalWrite (led2,HIGH);
+  digitalWrite (led3,LOW);
+  digitalWrite (led4,LOW);
+  digitalWrite (led5,LOW);
+  
+  delay(200);
+
+  digitalWrite (led1,LOW);
+  digitalWrite (led2,LOW);
+  digitalWrite (led3,HIGH);
+  digitalWrite (led4,LOW);
+  digitalWrite (led5,LOW);
+  
+  delay(200);
+
+  digitalWrite (led1,LOW);
+  digitalWrite (led2,LOW);
+  digitalWrite (led3,LOW);
+  digitalWrite (led4,HIGH);
+  digitalWrite (led5,LOW);
+  
+  delay(200);
+
+  digitalWrite (led1,LOW);
+  digitalWrite (led2,LOW);
+  digitalWrite (led3,LOW);
+  digitalWrite (led4,LOW);
+  digitalWrite (led5,HIGH);
+  
+  delay(200);
+}
