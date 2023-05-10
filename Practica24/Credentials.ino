2700def92b76e2d1c9dcc7d7fdb67b5e74c9a605
Guillermo Caselin Covarrubias <guillecascov@gmail.com>
2023-05-10 14:59:18 -0600

    Access credentials have been moved

---
+++ Practica24/Credentials.ino
@@ -0,0 +1,8 @@
+const char *SSID = "Repeater";
+const char *PASSWORD = "Kt#a&0Esh";
+
+const char *SEVER = "driver.cloudmqtt.com";
+const char *USERNAME = "erseastom";
+const char *MQTT_PASSWORD = "hNEMYkQpD";
+
+const int PORT = 18887;
